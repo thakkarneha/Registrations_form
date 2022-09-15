a3535b14a69e93a4657b16f13f9346a0dd9f4769
Neha Thakkar <112370511+thakkarneha@users.noreply.github.com>
2022-09-15 14:23:45 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+import './component/style.css';
 
 function App() {
+  const[cValue,setValues]=useState({
+    fName:"",
+email:"",
+password:""
+  })
+  const[formsub,setformsub]=useState(false)
+
+  const checkValues=(event)=>{
+    setValues({...cValue, [event.target.name]:event.target.value})
+
+  }
+ 
+  const CheckSuccessRate=(event)=>{
+    event.preventDefault();
+    setformsub(true);
+    
+    setValues({
+      fName:"",
+      email:"",
+      password:"",
+       })
+
+  }
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <form onSubmit={CheckSuccessRate}>
+    {formsub ?<span className="success">You have Successfully Submit the form.</span>:null}
+    <div className="form-group">
+    <label>Name</label>
+    <input type="text" className="form-control"  placeholder="Enter Name" name="fName" value={cValue.fName} onChange={checkValues} required/>
+    {/* //{formsub && !cValue.fName?<span>Please Enter The Name</span>:null} */}
     </div>
+  <div className="form-group">
+ <label >Email address</label>
+    <input type="email" className="form-control"  aria-describedby="emailHelp" placeholder="Enter email" name="email" value={cValue.email} onChange={checkValues} required/ >
+   {/* // {formsub && !cValue.email?<span>Please Enter The Email</span>:null} */}
+    </div>
+  <div className="form-group">
+    <label >Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"  name="password" value={cValue.password} onChange={checkValues} required/>
+   {/* // {formsub && !cValue.password? <span>Please Enter The Password</span>:null} */}
+  </div>
+ 
+  <button type="submit" className="btn btn-warning">Submit</button>
+</form>
+    </div>
+    </div>
+    </>
   );
 }
 
